8b04946d6a9e18f69f5e15f280373cb206399e61
rodrigo <rodrigoadcarvalho@gmail.com>
2024-05-23 12:12:07 -0300

    last version

---
+++ create_env.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+USERNAME=rcarvalh
+FIRSTNAME=rodrigo
+
+DB_HOST=mariadb
+DB_NAME=wordpress
+DB_ROOT=rootpass
+DB_USER=wpuser
+DB_PASS=wppass
+
+ADM_WP_NAME=${USERNAME}@42.fr
+ADM_WP_PASS=${USERNAME}
+ADM_WP_EMAIL=${USERNAME}@42.fr
+
+WP_USERNAME=${FIRSTNAME}@42.fr
+WP_USEREMAIL=${FIRSTNAME}@42.fr
+WP_USERPASS=${FIRSTNAME}
+
+ENV_CONTENT="# Domain:\n\
+DOMAIN_NAME=${USERNAME}.42.fr\n\
+\n\
+# Certificates:\n\
+CERT_=./requirements/nginx/tools/${USERNAME}.42.fr\n\
+KEY_=./requirements/nginx/tools/${USERNAME}.42.fr\n\
+\n\
+# Database:\n\
+DB_NAME=${DB_NAME}\n\
+DB_ROOT=${DB_ROOT}\n\
+DB_USER=${DB_USER}\n\
+DB_PASS=${DB_PASS}\n\
+DB_HOST=${DB_HOST}\n\
+\n\
+# Wordpress\n\
+WP_TITLE=${USERNAME}\n\
+WP_HOST=${USERNAME}.42.fr\n\
+\n\
+ADM_WP_NAME=${USERNAME}@42.fr\n\
+ADM_WP_PASS=${USERNAME}\n\
+ADM_WP_EMAIL=${USERNAME}@42.fr\n\
+\n\
+WP_USERNAME=${FIRSTNAME}@42.fr\n\
+WP_USEREMAIL=${FIRSTNAME}@42.fr\n\
+WP_USERPASS=${FIRSTNAME}\n\
+\n\
+# FTP\n\
+FTP_USR=ftpuser\n\
+FTP_PWD=ftppasss"
+
+echo ${USERNAME}
+echo ${ENV_CONTENT} > srcs/.env
+echo "File .env generated."
---
+++ info.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+USERNAME=rcarvalh
+FIRSTNAME=rodrigo
+
+DB_HOST=mariadb
+DB_NAME=wordpress
+DB_ROOT=rootpass
+DB_USER=wpuser
+DB_PASS=wppass
+
+ADM_WP_NAME=${USERNAME}@42.fr
+ADM_WP_PASS=${USERNAME}
+ADM_WP_EMAIL=${USERNAME}@42.fr
+
+WP_USERNAME=${FIRSTNAME}@42.fr
+WP_USEREMAIL=${FIRSTNAME}@42.fr
+WP_USERPASS=${FIRSTNAME}
+
+echo "Wordpress: https://${USERNAME}.42.fr"
+echo "           https://${USERNAME}.42.fr/wp-login.php"
+echo "    User: ${WP_USERNAME}"
+echo "    Email: ${WP_USEREMAIL}"
+echo "    Password: ${WP_USERPASS}"
+echo "    Admin: ${ADM_WP_NAME}"
+echo "    Email: ${ADM_WP_EMAIL}"
+echo "    Password: ${ADM_WP_PASS}"
+echo "Portainer: https://${USERNAME}.42.fr:9443"
+echo "Website: http://localhost http://${USERNAME}.42.fr"
+echo "Redis run-check: docker exec -it redis redis-cli monitor"
+echo "Adminer DB: http://localhost:8080"
+echo "    System: MySQL"
+echo "    Server: ${DB_HOST}"
+echo "    User: ${DB_USER}"
+echo "    Password: ${DB_PASS}"
+echo "    Database: ${DB_NAME}"
+echo "Vsftpd: FileZilla"
+echo "    Host: ftp://${USERNAME}.42.fr"
+echo "    Username: ${DB_USER}"
+echo "    Password: ${DB_PASS}"
+echo "    Port: 443"
